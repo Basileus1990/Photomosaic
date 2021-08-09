b4985e70a7e4f5a8e319c57379f242e0661fb5f4
Pawel Bogdanowicz <pawelb021@gmail.com>
2021-08-09 15:09:55 +0200

    Colmpeted the photomosaic

--- Classes/MosaicCreator.py
+++ Classes/MosaicCreator.py
@@ -4,56 +4,77 @@ from PIL import Image
 import numpy
 import ctypes
 import os
+import winsound
 
 
-# Takes the image path from the user,
-# devides it to squares 10x10pixels
-# searches for the most suitable image from the Images
+# devides it to squares 20x20pixels
+# searches for the most suitable images from the Images
 # Connects all images to form a mosaic
-# Displays the mosaic and saves it where user decides
+# Displays the mosaic and saves it where user decided
 class MosaicCreator:
-    def __init__(self, main_window):
-        self.main_window = main_window
-
-    # main method for creating mosaic. Calls all other needed to complete the proccess
-    def create_mosaic(self):
-        try:
-            #print(self.find_matching_image(['145', '25', '99']))
-            #return
-            image_path = self.get_user_image_path()
-            self.main_window.displayed_image_path = image_path
-            tiles, tiles_per_row = self.divide_image(image_path)
-
-            # creates a dictionary of tiles and of average color of these tiles
-            tiles_average = [self.calculate_average_color(tile) for tile in tiles]
-            for tile in tiles_average:
-                print(self.find_matching_image(tile))
-        except CreatorError:
+    def __init__(self, main_window, image_path, mosaic_path):
+        if image_path == '' or mosaic_path=='':
+            ctypes.windll.user32.MessageBoxW(0, 'You didn\'t select your image or your target directory. Please try again', '', 0)
             return
 
+        self.main_window = main_window
+        self.image_path = image_path
+        self.mosaic_path = mosaic_path
+
+        try:
+            self.main_window.change_displayed_image_from_thread(self.image_path)
+            tiles, original_image, tiles_per_row = self.divide_image()
+            self.number_of_tiles = len(tiles)
+            self.number_of_tiles_found = 0
+
+            # creates a dictionary of tiles and of average color of these tiles
+            average_tile_colors = [self.calculate_average_color(tile) for tile in tiles]
+            average_tile_paths = [self.find_matching_image(average_color) for average_color in average_tile_colors]
+
+            # mosaic = self.combine_tiles([Image.open(im_path) for im_path in average_tile_paths], original_image, tiles_per_row)
+            mosaic = self.combine_tiles(average_tile_paths, original_image, tiles_per_row)
+            self.save_mosaic(mosaic)
+
+        except Exception as e:
+            if str(e) != '':
+                with open(f'{os.getcwd()}\\Error Log.txt', 'a') as error_file:
+                    error_file.write(f'{e}\n\n')
+            else:
+                ctypes.windll.user32.MessageBoxW(0, 'Something went wrong. Please try again. If it doesn\'t help, try with another image', '', 0)
+            return
+
+
     # makes an dialog window pop out which enables the user select a file. Returns a path to this file
-    def get_user_image_path(self):
+    def get_user_image_path():
         root = tkinter.Tk()
         root.withdraw()
-        return filedialog.askopenfilename()
+        return filedialog.askopenfilename(title='Select your image')
 
-    # divides an image and returns a list of single tiles as Image objects
-    def divide_image(self, image_path):
+    # makes an dialog window pop out which enables the user select a folder. Returns a path to this filder
+    def get_user_mosaic_target_path():
+        root = tkinter.Tk()
+        root.withdraw()
+        return filedialog.askdirectory(title='Select a directory where you want to save your mosaic')
+
+    # Resises the image to make it divisible by size of tiles
+    # divides an image and returns a list of single tiles as Image objects,
+    # the original image and number of tiles per row
+    def divide_image(self):
         try:
-            image = Image.open(image_path)
+            image = Image.open(self.image_path)
         except Exception:
             ctypes.windll.user32.MessageBoxW(0, 'Couldn\'t open the image. Try selecting again or check if image is not corrupted', '', 0)
-            raise CreatorError()
+            raise Exception()
 
-        size = 10 # tile size
-        image = image.resize((round(image.size[0]/10)*10, round(image.size[1]/10)*10))
-        tiles_per_row = image.size[0]/10
+        size = 20 # tile size
+        image = image.resize((round(image.size[0]/size)*size, round(image.size[1]/size)*size))
+        tiles_per_row = round(image.size[0]/size)
         im = numpy.array(image)
         # Cuts an image into tiles and converts them back into an Image object.
         tiles = [Image.fromarray(numpy.uint8(im[x:x+size, y:y+size])).convert('RGB') for x in range(0,im.shape[0],size) for y in range(0,im.shape[1],size)]
-        return [tiles, tiles_per_row]
+        return [tiles, image, tiles_per_row]
 
-    # Returns average color of an image taken from the image_path, in RGB string list format
+    # Returns average color of an image as a RGB string list format
     def calculate_average_color(self, image):
         img = numpy.array(image)
         average_color_per_row = numpy.average(img, axis=0)
@@ -61,7 +82,13 @@ class MosaicCreator:
         average_color = [str(int(average_color[0])), str(int(average_color[1])), str(int(average_color[2]))]
         return average_color
 
+    # Searches the Image folder for suitable images and updates the progress bar
     def find_matching_image(self, base_av):
+        self.number_of_tiles_found += 1
+        new_progress_bar_value = round((self.number_of_tiles_found/self.number_of_tiles)*100)
+        if new_progress_bar_value != self.main_window.progress_bar_value:
+            self.main_window.change_progress_bar_value(new_progress_bar_value)
+
         if os.path.exists(f'{os.getcwd()}/Images/{base_av[0]}/{base_av[1]}/{base_av[2]}/{base_av[0]} {base_av[1]} {base_av[2]}.jpg'):
             return f'{os.getcwd()}/Images/{base_av[0]}/{base_av[1]}/{base_av[2]}/{base_av[0]} {base_av[1]} {base_av[2]}.jpg'
 
@@ -69,7 +96,8 @@ class MosaicCreator:
         while True:
             new_path = self.check_directories(deviation, f'{os.getcwd()}/Images', base_av)
             if new_path != '':
-                return new_path
+                print(new_path)
+                return f'{new_path}/{os.listdir(new_path)[0]}'
             deviation += 1
 
     # a recursive method which checks all directories within deviation and returns it's path if file is found
@@ -80,16 +108,14 @@ class MosaicCreator:
     # level -> which nested directory lever it is in
     # checked_dir -> contains all already checked directories. In those directories it only looks for directories which are a deviation away from it
     def check_directories(self, deviation, path, base_av, level=0, checked_dir=[]):
-        if not os.path.exists(path):
+        if not os.path.exists(path) or os.listdir(path) == []:
             return ''
-        elif os.listdir(path) != [] and 'jpg' in os.listdir(path)[0]:
+        elif 'jpg' in os.listdir(path)[0]:
             return path
         else:
             for i in range([0, deviation][path in checked_dir and level == 2], deviation+1):
                 new_path = self.check_directories(deviation, f'{path}/{str(int(base_av[level])+i)}',
                                                   base_av, level+1, checked_dir)
-                #if not str(int(base_av[level])+i) in checked_dir[level]:
-                #    checked_dir[level].append(str(int(base_av[level])+i))
                 if new_path != '':
                     return new_path
 
@@ -102,8 +128,38 @@ class MosaicCreator:
                 checked_dir.append(path)
             return ''
 
+    # Combines all tiles into a full image and returns it
+    def combine_tiles(self, average_tiles_paths, original_image, tiles_per_row):
+        total_width = original_image.size[0]
+        total_height = original_image.size[1]
+        with Image.open(average_tiles_paths[0]) as average_tile:
+            tile_size = average_tile.size[0]
+        new_image = Image.new('RGB', (total_width, total_height))
 
+        y_offset = 0
+        x_offset = 0
+        for i in range(len(average_tiles_paths)):
+            with Image.open(average_tiles_paths[i]) as average_tile:
+                new_image.paste(average_tile, (x_offset, y_offset))
+                x_offset += tile_size
+                if (i + 1) % tiles_per_row == 0:
+                    y_offset += tile_size
+                    x_offset = 0
 
-# used to exit the MosaicCreator after errors in methods
-class CreatorError(Exception):
-    pass
+        return new_image
+
+    # saves the mosaic in a folder given by the user and names it.
+    def save_mosaic(self, mosaic):
+        mosaic_name = 'Mosaic.jpg'
+        for i in range(1, 9999999):
+            if mosaic_name in os.listdir(self.mosaic_path):
+                mosaic_name = f'Mosaic{i}.jpg'
+                continue
+            break
+        self.mosaic_path = f'{self.mosaic_path}/{mosaic_name}'
+        mosaic.save(self.mosaic_path)
+        print(self.mosaic_path)
+        self.main_window.change_progress_bar_value(0)
+        self.main_window.change_displayed_image_from_thread(self.mosaic_path)
+
+        winsound.PlaySound("SystemQuestion", winsound.SND_ALIAS)
--- Main.py
+++ Main.py
@@ -1,20 +1,44 @@
 import kivy
-
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import StringProperty
-
-from Classes.GoogleImageDownloader import GoogleImageDownloader
+from kivy.properties import StringProperty, NumericProperty
+from kivy.clock import Clock
+import threading
 from Classes.MosaicCreator import MosaicCreator
 
 
 class MainWindow(BoxLayout):
     displayed_image_path = StringProperty('')
+    progress_bar_value = NumericProperty(0)
     def __init__(self, **kwargs):
-        super(MainWindow, self).__init__(*kwargs)
-        self.google_image_downloader = GoogleImageDownloader()
-        self.mosaic_creator = MosaicCreator(self)
+        self.image_change_trigger = Clock.create_trigger(self.change_displayed_image_from_thread)
+        self.progress_bar_value_trigger = Clock.create_trigger(self.change_progress_bar_value)
 
+        super(MainWindow, self).__init__(*kwargs)
+        self.mosaic_creator_thread = threading.Thread(target=MosaicCreator, args=(self, '',''), daemon=True)
+
+    # Will start creating an mosaic. Collecting paths from the user before entering the thread is necessary.
+    def start_mosaic_creator_thread(self):
+        if not self.mosaic_creator_thread.is_alive():
+            image_path = MosaicCreator.get_user_image_path()
+            mosaic_path = MosaicCreator.get_user_mosaic_target_path()
+            self.mosaic_creator_thread = threading.Thread(target=MosaicCreator, args=(self, image_path, mosaic_path), daemon=True)
+            self.mosaic_creator_thread.start()
+
+    # Changes displayed image from another thread.
+    def change_displayed_image_from_thread(self, image_path):
+        if threading.current_thread() is threading.main_thread():
+            self.displayed_image_path = self.new_displayed_image_path
+        else:
+            self.new_displayed_image_path = image_path
+            self.image_change_trigger()
+
+    def change_progress_bar_value(self, value):
+        if threading.current_thread() is threading.main_thread():
+            self.progress_bar_value = self.new_progress_bar_value
+        else:
+            self.new_progress_bar_value = value
+            self.progress_bar_value_trigger()
 
 class PhotomosaicApp(App):
     def build(self):
